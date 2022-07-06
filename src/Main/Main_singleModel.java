c7fad8260fc8065711c7ad2108d488e6f9964986
Elena <elenabock@gmx.de>
2022-07-06 22:41:13 +0200

    add method for removing a node in model and graph

--- src/Main/Main_singleModel.java
+++ src/Main/Main_singleModel.java
@@ -2,6 +2,7 @@ package Main;
 
 import java.awt.Panel;
 
+
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
@@ -9,9 +10,13 @@ import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
+
 import Main.ModelLoader.ResourceType;
+import graphVisualization.DataLoader;
 import graphVisualization.InstanceDiagrammLoader;
+import graphVisualization.Node;
 import graphVisualization.Visualizer;
+import userInterface.GraphManipulator;
 import userInterface.MainWindow;
 
 public class Main_singleModel {
@@ -34,29 +39,109 @@ public class Main_singleModel {
 		Resource srcResource = modelLoader.loadModelWithResourceHandler(ResourceType.Source);
 		
 	    InstanceDiagrammLoader dataSrc = new InstanceDiagrammLoader(srcResource, true);
+	    
+	    
+	    //EList<EObject> objects = srcResource.getContents();
+	    
+	    
+		/*for (EObject eObject : objects) {
+			
+			/*EList<EStructuralFeature> allEStructFeats = eObject.eClass().getEAllStructuralFeatures();
+			
+			for(EStructuralFeature esf : allEStructFeats)
+			{
+				System.out.println("blub");
+			    Object o = eObject.eGet(esf);
+
+			    if(true)
+			    {
+			    	Integer i = 0;
+			        eObject.eSet(esf, i);
+			        
+			    }
+			}
+			iterateModelHierarchical(eObject);
+			
+		}*/
+		
 		
 		
 		Visualizer visSrc = new Visualizer(shell, dataSrc);
 		
-		/*EList<EObject> objects = srcResource.getContents();
-		
-		int i = 0;
-		for (EObject eObject : objects) {
-			if (i==0) EcoreUtil.remove(eObject);
-			i++;
-			
-		}*/
-		
-		//visSrc.getGraph();
+		GraphManipulator manipulator = new GraphManipulator(visSrc, srcResource, dataSrc);
 	    
 	    shell.open();
 	    while (!shell.isDisposed()) {
+	    	manipulator.removeSelected();
+	    	//visSrc.getGraph().getModel().beginUpdate();
+	    	/*try {
+	    		Object[] selectedCells = visSrc.getGraph().getSelectionCells();
+	    		
+	    		if(selectedCells.length > 0) {
+	    			System.out.println("selected");
+	    			for (Object selected : selectedCells) {
+	    				visSrc.getGraph().getModel().remove(selected);
+	    				for (EObject eObject : objects) {
+	    					
+	    					/*EList<EStructuralFeature> allEStructFeats = eObject.eClass().getEAllStructuralFeatures();
+	    					
+	    					for(EStructuralFeature esf : allEStructFeats)
+	    					{
+	    						System.out.println("blub");
+	    					    Object o = eObject.eGet(esf);
+
+	    					    if(true)
+	    					    {
+	    					    	Integer i = 0;
+	    					        eObject.eSet(esf, i);
+	    					        
+	    					    }
+	    					}
+	    					iterateModelHierarchical(eObject, selected, dataSrc);
+	    				}
+					}
+	    			
+	    			
+	    			shell.redraw();
+	    			System.out.println("redrawn");
+	    		}
+	    	}
+	    	finally {
+	    		//visSrc.getGraph().getModel().endUpdate();
+	    	}*/
+	    	/*visSrc.getGraph().addListener(mxEvent.SELECT,function (sender, evt) {
+	    		mxEvent.isRightMouseButton(evt);
+	    	
+	    	}*/
+	    	
 	        if (!display.readAndDispatch()) {
 	            display.sleep();
+	        	
 	        }
+	        
 	    }
 	    display.dispose();
 
 	}
+	
+	/*private static void iterateModelHierarchical(EObject obj, Object comp, InstanceDiagrammLoader loader) {
+		for (EObject eobj : obj.eContents()) {
+			iterateModelHierarchical(eobj, comp, loader);
+		}
+		
+		mxCell c = (mxCell) comp;
+		if(obj.toString().equals(c.getId())) {
+			System.out.println("equal found");
+			EcoreUtil.remove(obj); //delete wirft Nullpointerexception, aber so wird Kante nicht gelöscht
+			System.out.println("removed");
+			for (Node nodeElement : loader.nodes) {
+				if(nodeElement.id.equals(obj.toString())) {
+					loader.nodes.remove(nodeElement);
+				}
+			}
+		}
+		System.out.println(obj.toString());
+		System.out.println(c.getId());
+	}*/
 	
 }
