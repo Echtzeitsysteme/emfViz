de6848101b94d90262fb57f2d92b9cc33a69058d
Jordan-l <jordan-l@freenet.de>
2022-07-16 16:26:18 +0200

    adjust emfViz/src/visualizer so that marek's orginial code also works

--- emfViz/src/Example/exampleMain.java
+++ emfViz/src/Example/exampleMain.java
@@ -1,90 +1,93 @@
-package Example;
+package GraphVisualizer;
 
-public class exampleMain {
-	import java.io.File;
+import java.io.File;
 
-	import org.eclipse.emf.common.util.URI;
-	import org.eclipse.emf.ecore.resource.ContentHandler;
-	import org.eclipse.emf.ecore.resource.Resource;
-	import org.eclipse.emf.ecore.resource.ResourceSet;
-	import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-	//Imports require org.eclipse.swt
-	import org.eclipse.swt.layout.FillLayout;
-	import org.eclipse.swt.widgets.Display;
-	import org.eclipse.swt.widgets.Shell;
-	import org.emoflon.smartemf.persistence.SmartEMFResourceFactoryImpl;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ContentHandler;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+//Imports require org.eclipse.swt
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.emoflon.smartemf.persistence.SmartEMFResourceFactoryImpl;
 
-	import HospitalExample.HospitalExamplePackage;
-	import graphVisualization.InstanceDiagrammLoader;
-	// First add emfViz project to your's project build path via 'Properties' -> 'Java Build Path' -> 'Projects' -> 'Classpath' -> 'Add' 
-	import graphVisualization.Visualizer;
+import HospitalExample.HospitalExamplePackage;
+import graphVisualization.InstanceDiagrammLoader;
+// First add emfViz project to your's project build path via 'Properties' -> 'Java Build Path' -> 'Projects' -> 'Classpath' -> 'Add' 
+import graphVisualization.Visualizer;
 
-	public class vis {
+public class Main_emfViz {
+	
+	
+	
+	
+	public static void main(String[] args) {
 		
-		public static void main(String[] args) {
-			
-	        Display display = new Display();
-	 
-	        Shell shell = new Shell(display);
-	      
-	        shell.setLayout(new FillLayout());
-	        
-	        //Visualization is optimized for this shell size
-	        shell.setSize(shell.getMonitor().getClientArea().width, shell.getMonitor().getClientArea().height);
-	        
-	        //Example for loading an instance model diagramm
-	        
-	        URI base = URI.createPlatformResourceURI("/", true);
-	        
-	        //Assuming your instance model is contained in an .xmi file, path can be adjusted accordingly
-			URI uri =  URI.createURI("hospital.xmi");
-			
-			ResourceSet rs = new ResourceSetImpl();
-			rs.getResourceFactoryRegistry().getExtensionToFactoryMap()
-			.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new SmartEMFResourceFactoryImpl("../"));
-			
-			try {
-				rs.getURIConverter().getURIMap().put(URI.createPlatformResourceURI("/", true), URI.createFileURI(new File("../").getCanonicalPath() + File.separator));
-				}
-			catch(Exception e) {
-				System.out.print(e.getMessage());
+        Display display = new Display();
+ 
+        Shell shell = new Shell(display);
+      
+        shell.setLayout(new FillLayout());
+        
+        //Visualization is optimized for this shell size
+        shell.setSize(shell.getMonitor().getClientArea().width, shell.getMonitor().getClientArea().height);
+        
+        //Example for loading an instance model diagramm
+        
+        URI base = URI.createPlatformResourceURI("/", true);
+        
+        //Assuming your instance model is contained in an .xmi file, path can be adjusted accordingly
+		URI uri =  URI.createURI("/HospitalExample/hospital.xmi");
+		
+		ResourceSet rs = new ResourceSetImpl();
+		rs.getResourceFactoryRegistry().getExtensionToFactoryMap()
+		.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new SmartEMFResourceFactoryImpl("../"));
+		
+		try {
+			rs.getURIConverter().getURIMap().put(URI.createPlatformResourceURI("/", true), URI.createFileURI(new File("../").getCanonicalPath() + File.separator));
 			}
-			
-			Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-			reg.getExtensionToFactoryMap().put("xmi", new SmartEMFResourceFactoryImpl("../"));
-			
-			rs.getPackageRegistry().put(HospitalExamplePackage.eINSTANCE.getNsURI(), HospitalExamplePackage.eINSTANCE);
-			
-
-			
-			Resource instanceModel = rs.createResource(uri, ContentHandler.UNSPECIFIED_CONTENT_TYPE);
-			try {
-			instanceModel.load(null);
-			}
-			catch(Exception e) {
-				System.out.print(e.getMessage());
-			}
-	        
-			//Create an instace of the InstaceDiagrammLoader class with the loaded resource
-			InstanceDiagrammLoader data = new InstanceDiagrammLoader(instanceModel);
-
-	        //Create instance of the graph visualizer
-			//Visualization will be loaded by its constructor
-	        Visualizer vis = new Visualizer(shell, data);
-	        
-	        shell.open();
-	        while (!shell.isDisposed()) {
-	            if (!display.readAndDispatch()) {
-	                display.sleep();
-	            }
-	        }
-	        display.dispose();
-	        
-
-	       
+		catch(Exception e) {
+			System.out.println(e.getMessage());
 		}
+		
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		reg.getExtensionToFactoryMap().put("xmi", new SmartEMFResourceFactoryImpl("../"));
+		
+		rs.getPackageRegistry().put(HospitalExamplePackage.eINSTANCE.getNsURI(), HospitalExamplePackage.eINSTANCE);
+		
 
+		
+		Resource instanceModel = rs.createResource(uri, ContentHandler.UNSPECIFIED_CONTENT_TYPE);
+		try {
+		instanceModel.load(null);
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+        
+		//Create an instace of the InstaceDiagrammLoader class with the loaded resource
+		InstanceDiagrammLoader data = new InstanceDiagrammLoader(instanceModel, true);
+
+        //Create instance of the graph visualizer
+		//Visualization will be loaded by its constructor
+        Visualizer vis = new Visualizer(data, shell);
+        
+        vis.init();
+        
+        shell.open();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+        display.dispose();
+        
+
+       
 	}
 
-
 }
+
+
---
+++ emfViz/src/graphVisualization/Visualizer_TGGDemonstrator.java
@@ -0,0 +1,44 @@
+package graphVisualization;
+
+import java.awt.Frame;
+import java.awt.geom.Point2D;
+
+import org.eclipse.swt.graphics.Rectangle;
+
+import com.mxgraph.model.mxGraphModel;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+
+
+public class Visualizer_TGGDemonstrator extends Visualizer {
+
+private Frame frame;
+	
+	
+	public Visualizer_TGGDemonstrator(DataLoader dataLoader, Frame frame, Rectangle r) {
+		super(dataLoader, null);
+	
+		shellBounds = r;
+		this.frame = frame;		
+		
+	}	
+	
+	@Override
+	public void init () {
+		
+		this.dataLoader.loadData();
+		
+		defaultNodePosition = new Point2D.Double(((double) shellBounds.width) * 0.5 - defaultNodeWidth * 0.5 , ((double) shellBounds.height) * 0.5 - defaultNodeHeight * 0.5);
+		
+		graph = new mxGraph();
+		graphModel = ((mxGraphModel)graph.getModel());
+		
+		addStyles();
+		insertDataIntoGraph();
+		setUpLayout();
+		runLayout();
+		
+		graphComponent = new mxGraphComponent(graph);
+		frame.add(graphComponent);
+	}
+}
