b1920a2cb59c7025d1b87f07cf80f6d2f145654f
Jordan-l <jordan-l@freenet.de>
2022-08-04 21:07:37 +0200

    Anpassungen im Model Generation Proccess

--- TGGDemonstrator/src/tggDemonstrator/ModelLoader_MODELGEN.java
+++ TGGDemonstrator/src/tggDemonstrator/ModelLoader_MODELGEN.java
@@ -3,6 +3,8 @@ package tggDemonstrator;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Function;
 
 import org.emoflon.ibex.tgg.operational.matches.ITGGMatch;
@@ -17,10 +19,11 @@ public class ModelLoader_MODELGEN extends TGGDemonstrator {
 	
 	private Function<String[], MODELGEN> modelgen_demonstrator;
 	private MODELGEN modelgen;
-	private String[] paths	= new String[2];
+	private String[] paths	= new String[3];
 	
 	private ModelgenThread thread;
 	private int ruleIndex;
+	private Set<ITGGMatch> matches;
 	
 	
 	public ModelLoader_MODELGEN (Function<String[], MODELGEN> modelgen, String pP, String wP) {	
@@ -39,6 +42,7 @@ public class ModelLoader_MODELGEN extends TGGDemonstrator {
 		// TODO Auto-generated method stub
 		paths[0] = pathSrc;
 		paths[1] = pathTrg;
+		paths[2] = "false";
 		
 		modelgen = modelgen_demonstrator.apply(paths);
 		
@@ -56,6 +60,7 @@ public class ModelLoader_MODELGEN extends TGGDemonstrator {
 		
 		paths[0] = projectPath + "/instances/src.xmi";
 		paths[1] = projectPath + "/instances/trg.xmi";
+		paths[2] = "false";
 		
 		modelgen = modelgen_demonstrator.apply(paths);
 		
@@ -75,20 +80,19 @@ public class ModelLoader_MODELGEN extends TGGDemonstrator {
 		
 		paths[0] = projectPath + "/instances/src.xmi";
 		paths[1] = projectPath + "/instances/trg.xmi";
+		paths[2] = "true";
 		
 		modelgen = modelgen_demonstrator.apply(paths);
 		
 		options = modelgen.getOptions();
 		resourceHandler = modelgen.getResourceHandler();
 		
+		source = resourceHandler.getSourceResource();
+		target = resourceHandler.getTargetResource();
 		
-		// create empty models
-		source = resourceHandler.createResource(paths[0]);
 		
-		target = resourceHandler.createResource(paths[1]);
+		// create empty models  -- falsches vorgehen! --> boolean im constructor im modelgen zur unterscheidung zw. load und createResource
 		
-	
-
 		
 		
 		// set basic stop criterion
@@ -99,7 +103,9 @@ public class ModelLoader_MODELGEN extends TGGDemonstrator {
 		
 		// Start new thread
 		thread = new ModelgenThread(modelgen);
+		thread.setName("Modelgen Thread");
 		thread.start();
+		
 		System.out.println("Model generation process is running on thread " + thread.getId());
 		
 	}
@@ -127,17 +133,19 @@ public class ModelLoader_MODELGEN extends TGGDemonstrator {
 	 */
 	public String[] getRuleNames() {
 		
-		String[] ruleNames = new String[modelgen.getMatchContainer().getMatches().size()];
+		matches = thread.getMatches();
+		
+		String[] ruleNames = new String[matches.size()];
 		int i = 0;
 		
 		
-		for (ITGGMatch m : modelgen.getMatchContainer().getMatches()) {
+		for (ITGGMatch m : matches) {
 			ruleNames[i] = m.getRuleName();
 			
 			i++;
 		}
 		
-		return ruleNames;
+		return ruleNames;	
 	}
 	
 	/*
@@ -165,6 +173,8 @@ class ModelgenThread extends Thread{
 	private MODELGEN modelgen;
 	private int ruleIndex;
 	
+	private Set<ITGGMatch> matches = new HashSet<> ();
+	
 	public ModelgenThread(MODELGEN m) {
 		this.modelgen = m;
 		ruleIndex = -1;
@@ -173,7 +183,7 @@ class ModelgenThread extends Thread{
 	@Override
 	public void run(){
 		
-		initilaizeModelgenStart();
+		initializeModelgenStart();
 		
 		runModelgeneration();
 		
@@ -197,41 +207,41 @@ class ModelgenThread extends Thread{
 		return true;
 	}
 	
-	public void initilaizeModelgenStart() {
+	public void initializeModelgenStart() {
 		modelgen.setUpdatePolicy((IUpdatePolicy) new IUpdatePolicy(){
 			@Override
 			public ITGGMatch chooseOneMatch(ImmutableMatchContainer matchContainer) {
 				
+
 				System.out.println("Thread sleeps again for a very long time!");
 				
 				try {
+					
+					matches = matchContainer.getMatches();
+					
 					sleep(Long.MAX_VALUE);
+					
 				} catch (InterruptedException e) {
 					
-					System.out.println("RuleIndex: " + ruleIndex);
 					
-					if (ruleIndex >= 0) {
-						ITGGMatch match = null;
+					ITGGMatch match = null;
+					
+					if (ruleIndex >= 0 && matches.size() - 1 == ruleIndex) {
 						
-						System.out.println(matchContainer.getMatches().toString());
+						//System.out.println(matchContainer.getMatches().size());
 						
-						for (int i = 0; i <= ruleIndex; i++) {
-							
-							if (matchContainer.getMatches().iterator().hasNext())
-								match = matchContainer.getMatches().iterator().next();
-							else
-								System.out.println("ERROR !!!! - selected rule does not exist");
-						}
+						Object[] t = matches.toArray();
+						match = (ITGGMatch)t[ruleIndex];
+			
 						
-						System.out.println("the rule " + match.getRuleName() + " will be performed");
-						
-						return match;
 					}else {
 						//if no rule is selected then just use the next one
-						ITGGMatch match = matchContainer.getNext();
-						
-						System.out.println("the rule " + match.getRuleName() + " will be performed");
+						match = matchContainer.getNext();
 					}
+					
+					System.out.println("the rule " + match.getRuleName() + " will be performed");
+					
+					return match;
 				}
 				
 				return null;
@@ -253,6 +263,10 @@ class ModelgenThread extends Thread{
 	
 	public  void setRuleIndex(int i) {
 		ruleIndex = i;
+	}
+	
+	public Set<ITGGMatch> getMatches(){
+		return matches;
 	}
 }
 
