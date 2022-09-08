71ad689ab5e462abafbfe140dfb7513521c72257
Lars Fritsche <arikae00@jesitec.de>
2022-09-08 13:15:04 +0200

    fixed array out of bounds exception and improved layouting

--- emfViz/src/graphVisualization/CustomFastOrganicLayout.java
+++ emfViz/src/graphVisualization/CustomFastOrganicLayout.java
@@ -2,33 +2,29 @@ package graphVisualization;
 
 import com.mxgraph.layout.mxFastOrganicLayout;
 import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxICell;
 import com.mxgraph.view.mxGraph;
 
-public class CustomFastOrganicLayout extends mxFastOrganicLayout{
-	
-	public CustomFastOrganicLayout(mxGraph graph)
-	{
+public class CustomFastOrganicLayout extends mxFastOrganicLayout {
+
+	public CustomFastOrganicLayout(mxGraph graph) {
 		super(graph);
-	}
-	
-	@Override
-	public boolean isVertexIgnored(Object vertex) {
-		var ignored = super.isVertexIgnored(vertex);
-		
-		if(vertex instanceof mxCell cell) {
-			var value = cell.getValue();
-			
-			if(value instanceof Node node)
-				ignored = ignored || node.ignored;
-		}
-		return ignored;
 	}
 
 	@Override
-	public void execute(Object parent)
-	{
-		
-		
+	public boolean isVertexMovable(Object vertex) {
+		// TODO Auto-generated method stub
+		if(vertex instanceof mxCell cell) {
+			if(cell.getValue() instanceof Node node) {
+				return !node.ignored;
+			}
+		}
+		return super.isVertexMovable(vertex);
+	}
+	
+	@Override
+	public void execute(Object parent) {
+
 		super.execute(parent);
 	}
 }
